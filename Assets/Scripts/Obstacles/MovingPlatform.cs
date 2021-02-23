1355d7e649a917f66309e2ea404a04ecb70ddc24
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-02-23 11:54:06 +0000

    Added a falling platform and refactored the obstacle code into a namespace and folder

---
+++ Assets/Scripts/Obstacles/MovingPlatform.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+namespace Polar.Obstacles
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        [SerializeField] PlatformRoute platformRoute;
+        [SerializeField] float platformSpeed = 2f;
+        [SerializeField] float _waypointLeeway = 2f;
+
+        private Transform _transform;
+        private int _waymarkCounter = 0;
+        private bool hasWaymarks = false;
+
+        Waymark[] _waymarks;
+        private Vector3 currentTarget;
+
+        private void Awake()
+        {
+            _waymarks = platformRoute.GetComponentsInChildren<Waymark>();
+            _transform = transform;
+
+            //Setting the initial waymark (if there are any)
+            if (_waymarks.Length == 0)
+            {
+                currentTarget = _transform.position;
+            }
+            else
+            {
+                currentTarget = _waymarks[_waymarkCounter].transform.position;
+                hasWaymarks = true;
+            }
+        }
+
+        private void Update()
+        {
+            if(hasWaymarks)
+            {
+                CheckDistanceFromTarget();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if(hasWaymarks)
+            {
+                transform.position = Vector3.Lerp(_transform.position, currentTarget, platformSpeed * Time.deltaTime);
+            }
+        }
+
+        private void UpdateTargetWaymark()
+        {
+            _waymarkCounter++;
+
+            if (_waymarkCounter == _waymarks.Length)
+            {
+                _waymarkCounter = 0;
+            }
+
+            currentTarget = _waymarks[_waymarkCounter].transform.position;
+        }
+
+        private void CheckDistanceFromTarget()
+        {
+            float distFromTarget = Vector3.Distance(_transform.position, currentTarget);
+            if (distFromTarget <= _waypointLeeway)
+            {
+                UpdateTargetWaymark();
+            }
+        }
+
+    }
+}
