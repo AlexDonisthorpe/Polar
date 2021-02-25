656a18c832e788dcb14470fc7697ed37d2782655
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-02-25 00:06:56 +0000

    Crates and Walls!

---
+++ Assets/DestructableWall.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Polar.Obstacles
+{
+    public class DestructableWall : MonoBehaviour
+    {
+        private CrashCrate[] _boxes;
+        private bool hasBlownUp = false;
+
+        [SerializeField] float boxExplosionForce = 500f;
+
+        private void Awake()
+        {
+            _boxes = GetComponentsInChildren<CrashCrate>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+           if (other.CompareTag("Player"))
+           {
+                foreach (var box in _boxes)
+                {
+                    box.BlowUp(boxExplosionForce);
+                }
+
+                GetComponent<BoxCollider>().enabled = false;
+                GetComponentInParent<BoxCollider>().enabled = false;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/CrashCrate.cs
@@ -0,0 +1,48 @@
+﻿namespace Polar.Obstacles
+{
+    using UnityEngine;
+
+    public class CrashCrate : MonoBehaviour
+    {
+        [Header("Whole Create")]
+        public MeshRenderer wholeCrate;
+        public BoxCollider boxCollider;
+
+        [Header("Fractured Create")]
+        public GameObject fracturedCrate;
+
+        [Header("Test")]
+        [SerializeField] bool testing;
+
+        private bool hasExploded = false;
+
+        private void Update()
+        {
+            if(testing && !hasExploded)
+            {
+                BlowUp(500);
+                hasExploded = true;
+            }
+        }
+
+        [ContextMenu("Test")]
+        public void BlowUp(float explosionForce)
+        {
+            wholeCrate.enabled = false;
+            fracturedCrate.SetActive(true);
+            Boom(explosionForce);
+        }
+
+        private void Boom(float explosionForce)
+        {
+            Rigidbody[] allRigidBodies = fracturedCrate.GetComponentsInChildren<Rigidbody>();
+            if (allRigidBodies.Length > 0)
+            {
+                foreach (var body in allRigidBodies)
+                {
+                    body.AddExplosionForce(explosionForce, transform.position, 1);
+                }
+            }
+        }
+    }
+}
