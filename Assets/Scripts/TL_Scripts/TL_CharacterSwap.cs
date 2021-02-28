a84f3b75d8e5eb19583afa482c98edab7f8a02ea
Tony Luu <tonyluu@hotmail.co.uk>
2021-02-28 21:59:46 +0000

    Forgot to include comments

--- Assets/Scripts/TL_Scripts/TL_CharacterSwap.cs
+++ Assets/Scripts/TL_Scripts/TL_CharacterSwap.cs
@@ -10,21 +10,32 @@ public class TL_CharacterSwap : MonoBehaviour
     private bool CharacterToggle;
 
 
+    //Swapping the materials for the characters
     void SwapMaterial()
     {
+        //If the tab key is pressed
         if (Input.GetKeyDown(KeyCode.Tab))
         {
+            //Toggle the boolean
             CharacterToggle = !CharacterToggle;
+
+            //If the toggle is true
             if (CharacterToggle)
             {
+                //Set the male hair active and deactivate the female hair
                 MaleHair.SetActive(true);
                 FemaleHair.SetActive(false);
+
+                //Change to the male character material from the skinned mesh renderer
                 CharacterRenderer.material = MaleCharacterMaterial;
             }
-            else
+            else      //If the toggle is false
             {
+                //Set the male hair inactive and activate the female hair
                 MaleHair.SetActive(false);
                 FemaleHair.SetActive(true);
+
+                //Change to the female character material from the skinned mesh renderer
                 CharacterRenderer.material = FemaleCharacterMaterial;
             }
         }
