45ba4b1deeaab622b07a25d14f7934df0cfe1aed
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-03-08 21:13:08 +0000

    Applied an animation mask for the throw/grab animations so the legs can continue to operate even with the pickup/throw animations

--- Assets/Scripts/TL_Scripts/TL_ThrowObjects.cs
+++ Assets/Scripts/TL_Scripts/TL_ThrowObjects.cs
@@ -18,11 +18,13 @@ public class TL_ThrowObjects : MonoBehaviour
         //If the throw button is pressed while the character is holding an object
         if (Input.GetKeyDown(KeyCode.T) && GrabObjectsScript.ReturnPickedUpObject() != null)
         {
-            //Set the trigger to false
-            CharacterAnimator.SetBool("IsGrabbing", false);
 
             //Set the trigger to true
             CharacterAnimator.SetBool("IsThrowing", true);
+            
+            //Set the trigger to false
+            CharacterAnimator.SetBool("IsGrabbing", false);
+
 
             //Obtain the rigidbody from the picked up object
             Rigidbody ObjectRigidbody = GrabObjectsScript.ReturnPickedUpObject().GetComponent<Rigidbody>();
